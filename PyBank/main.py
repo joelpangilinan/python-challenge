d808eb35c104e078b5bbac57bd339c9eedc945c0
kombart <joel_pangilinan@yahoo.com>
2022-12-17 22:53:20 -0800

    updates on printing for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,8 +19,9 @@ filepath = os.path.join("C:/Users/Joel/Documents/UC-Berkeley-Boot-Camp/Weekly-as
 # To determine the total number of months included in the dataset, you have to count the number of rows minus the header
 with open(filepath) as budget_file:
     read_budget = csv.reader(budget_file, delimiter=',')
-    number_of_months = len(list(read_budget))
-    print("Total Months: ", number_of_months - 1)
+    months = len(list(read_budget))
+    number_of_months = months - 1
+    #print(number_of_months)
 
 # To determine the net total amount of "Profit/Losses" over the entire period, sum up the total the column "Profit/Losses" minus the header
 with open(filepath) as budget_file:
@@ -30,13 +31,13 @@ with open(filepath) as budget_file:
     header = next(read_budget)
     for row in read_budget:
         net_total += int(row[1])
-    print("Total: ", net_total)
+    #print("Total: ", net_total)
 
 # The changes in "Profit/Losses" over the entire period, and then the average of those changes
 total_months = number_of_months - 1
 total_changes = net_total
 average_change = float(net_total / total_months)
-print("Average Change: $", average_change)
+#print("Average Change: $", average_change)
 
 # The greatest increase in profits (date and amount) over the entire period
 with open(filepath) as budget_file:
@@ -50,8 +51,8 @@ with open(filepath) as budget_file:
     values = [x for x in profit]
     max_profit = max(values)
     min_profit = min(values)
-    print(max_profit)
-    print(min_profit)
+    #print(max_profit)
+    #print(min_profit)
 
 # Determine the date that matches the max value
 with open(filepath) as budget_file:
@@ -61,7 +62,7 @@ with open(filepath) as budget_file:
     for row in read_budget:
         if int(row[1]) == max_profit:
             max_date = (row[0])
-            print(max_date)
+            #print(max_date)
 
 # Determine the date that matches the min value
 with open(filepath) as budget_file:
@@ -71,5 +72,15 @@ with open(filepath) as budget_file:
     for row in read_budget:
         if int(row[1]) == min_profit:
             min_date = (row[0])
-            print(min_date)
+            #print(min_date)
+
+# Send the results to the analysis report file
+print("Financial Analysis")
+print("--------------------------------")
+print("Total Months: ", number_of_months)
+print("Total: $", net_total)
+print("Average Change: $", average_change)
+print("Greatest Increase in Profits: ", max_date, " ($", max_profit,")")
+print("Greatest Decrease in Profits: ", min_date, " ($", min_profit,")")
+
 
