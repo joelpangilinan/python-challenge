443e90e9d53dd57a91c693919734ebbd2c459ea2
kombart <joel_pangilinan@yahoo.com>
2022-12-18 15:23:17 -0800

    update formatting on results for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,18 +16,18 @@ ballot_file_path = os.path.join("C:/Users/Joel/Documents/UC-Berkeley-Boot-Camp/W
 with open(ballot_file_path) as ballot_file:
     read_ballot_file = csv.reader(ballot_file, delimiter=',')
     total_number_of_votes_cast = len(list(read_ballot_file)) - 1
-    print("Total votes cast: ", total_number_of_votes_cast)
+    #print("Total votes cast: ", total_number_of_votes_cast)
 
 # A complete list of candidates who received votes
-with open(ballot_file_path) as ballot_file:
+with open(ballot_file_path, encoding='utf-8') as ballot_file:
     read_ballot_file = csv.reader(ballot_file, delimiter=',')
     # Exclude the header
     header = next(read_ballot_file)
     candidate_list = []
     for row in read_ballot_file:
         candidate_list.append(row[2])
-        candidates = sorted(list(set(candidate_list)))
-    print(candidates)
+    candidates = sorted(list(set(candidate_list)))
+    #print(candidates)
 
 # The total number of votes each candidate won
 with open(ballot_file_path) as ballot_file:
@@ -49,17 +49,17 @@ with open(ballot_file_path) as ballot_file:
     total_votes_cast_for_charles = len(votes_for_charles)
     total_votes_cast_for_diana = len(votes_for_diana)
     total_votes_cast_for_raymon = len(votes_for_raymon)
-    print("Total votes for Charles: ", total_votes_cast_for_charles)
-    print("Total votes for Diana: ", total_votes_cast_for_diana)
-    print("Total votes for Raymon: ", total_votes_cast_for_raymon)
+    #print("Total votes for Charles: ", total_votes_cast_for_charles)
+    #print("Total votes for Diana: ", total_votes_cast_for_diana)
+    #print("Total votes for Raymon: ", total_votes_cast_for_raymon)
     
 # The percentage of votes each candidate won
 percent_vote_for_charles = "{:.3%}".format(total_votes_cast_for_charles/total_number_of_votes_cast)
 percent_vote_for_diana = "{:.3%}".format(total_votes_cast_for_diana/total_number_of_votes_cast)
 percent_vote_for_raymon = "{:.3%}".format(total_votes_cast_for_raymon/total_number_of_votes_cast)
-print("Percentage votes for charles: ", percent_vote_for_charles)
-print("Percentage votes for diana: ", percent_vote_for_diana)
-print("Percentage votes for raymon: ", percent_vote_for_raymon)
+#print("Percentage votes for charles: ", percent_vote_for_charles)
+#print("Percentage votes for diana: ", percent_vote_for_diana)
+#print("Percentage votes for raymon: ", percent_vote_for_raymon)
 
 # The winner of the election based on popular vote
 winner_dict = {}
@@ -67,7 +67,7 @@ winner_dict[candidates[0]] = total_votes_cast_for_charles
 winner_dict[candidates[1]] = total_votes_cast_for_diana
 winner_dict[candidates[2]] = total_votes_cast_for_raymon
 winning_candidate = max(zip(winner_dict.values(), winner_dict.keys()))[1]
-print(winning_candidate)
+#print(winning_candidate)
 
 #Election Results
 #  -------------------------
@@ -81,13 +81,13 @@ print(winning_candidate)
 #  -------------------------
 
 # Print the results
-print('Election Results')
-print('---------------------------------')
-print('Total Votes: ', total_number_of_votes_cast)
-print('---------------------------------')
-print(candidates[0], ': ', percent_vote_for_charles, ' (', total_votes_cast_for_charles, ')')
-print(candidates[1], ': ', percent_vote_for_diana, ' (', total_votes_cast_for_diana, ')')
-print(candidates[2], ': ', percent_vote_for_raymon, ' (', total_votes_cast_for_raymon, ')')
-print('---------------------------------')
-print('Winner: ', winning_candidate)
-print('---------------------------------')
+print(f"Election Results")
+print(f"---------------------------------")
+print(f"Total Votes: ", total_number_of_votes_cast)
+print(f"---------------------------------")
+print(f"{candidates[0]}: {percent_vote_for_charles} ({total_votes_cast_for_charles})")
+print(f"{candidates[0]}: {percent_vote_for_diana} ({total_votes_cast_for_diana})")
+print(f"{candidates[0]}: {percent_vote_for_raymon} ({total_votes_cast_for_raymon})")
+print(f"---------------------------------")
+print(f"Winner: {winning_candidate}")
+print(f"---------------------------------")
